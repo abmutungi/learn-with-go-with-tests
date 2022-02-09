570980a3fafcf8237f1f5cc1b2d2137da02c09ad
abmutungi <79641169+abmutungi@users.noreply.github.com>
2022-02-09 09:35:25 +0000

    updated

---
+++ 7-french/bonjour_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	// you can declare functions inside other functions and assign them to variables
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+	t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Francesca", "Spanish")
+		want := "Hola, Francesca"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Noemie", "French")
+		want := "Bonjour, Noemie"
+		assertCorrectMessage(t, got, want)
+	})
+}
